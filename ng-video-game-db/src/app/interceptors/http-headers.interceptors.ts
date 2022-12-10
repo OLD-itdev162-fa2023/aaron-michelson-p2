9585e53151a3dde916b30c77619ee349bbefe2d2
ajmichel4000 <93304691+ajmichel4000@users.noreply.github.com>
2022-12-10 10:40:11 -0600

    Completed Project 2

---
+++ ng-video-game-db/src/app/interceptors/http-headers.interceptors.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': '7e1c1ae055msh9db426888492ce0p1c51a9jsn9b1ff7201f01',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+      },
+      setParams: {
+        key: 'e40e743af2c94b0c916a8aa618fb4473',
+      }
+    });
+    return next.handle(req);
+  }
+}
